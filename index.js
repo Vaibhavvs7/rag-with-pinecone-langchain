e8ef818afb522551d583f093522befcf03faf153
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-08-10 10:57:20 +0530

    feat: implement document chunking for PDF indexing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
-
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 async function indexDocument(){
     // Load the PDF document
@@ -7,5 +7,14 @@ async function indexDocument(){
     const pdfLoader = new PDFLoader(PDF_PATH);
     const rawDocs = await pdfLoader.load();
 
+    // Split the document into smaller chunks
+    const textSplitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+    });
+    const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+
+    
+    
 }
 indexDocument();
