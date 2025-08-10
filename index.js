889e183f8fce6181fd241dce4b43f553f2d8d33d
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-08-10 11:13:02 +0530

    feat: enhance document indexing process with logging and PineconeStore integration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
 import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
 
 
 async function indexDocument(){
@@ -11,6 +12,7 @@ async function indexDocument(){
     const PDF_PATH = './dsa.pdf';
     const pdfLoader = new PDFLoader(PDF_PATH);
     const rawDocs = await pdfLoader.load();
+    console.log("PDF loaded")
 
     // Split the document into smaller chunks
     const textSplitter = new RecursiveCharacterTextSplitter({
@@ -18,16 +20,26 @@ async function indexDocument(){
         chunkOverlap: 200,
     });
     const chunkedDocs = await textSplitter.splitDocuments(rawDocs);
+    console.log("Chunking completed")
 
     // Create embeddings for the chunks
     const embeddings = new GoogleGenerativeAIEmbeddings({
         apiKey: process.env.GEMINI_API_KEY,
         model: 'text-embedding-004',
     });
+    console.log("Embeddings created")
     
     // Database configuration
     //initialize Pinecone client
     const pinecone = new Pinecone();
     const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+    console.log("Pinecone client initialized")
+
+    //Langchain(chunking, embedding, and indexing(database))
+     await PineconeStore.fromDocuments(chunkedDocs, embeddings, {
+        pineconeIndex,
+        maxConcurrency: 5,
+    });
+    console.log("Document stored in Pinecone")
 }
 indexDocument();
