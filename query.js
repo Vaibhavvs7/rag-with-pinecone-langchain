d3da9cf49fbeb21539085338c8930062a1411540
Vaibhavvs7 <vvsvaibhav07@gmail.com>
2025-08-10 11:21:34 +0530

    feat: integrate Pinecone client for document search in querying process

--- query.js
+++ query.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import readlineSync from 'readline-sync';
 import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
-
+import { Pinecone } from '@pinecone-database/pinecone';
 
 
 async function chatting(question) {
@@ -13,7 +13,21 @@ async function chatting(question) {
         model: 'text-embedding-004',
     });
     
-    const queryVector = await embeddings.embedQuery(question);   
+    const queryVector = await embeddings.embedQuery(question);   // query vector embedding
+
+    // Initialize Pinecone client connection and Search Relevant document into vector DB
+    const pinecone = new Pinecone();
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+
+    const searchResults = await pineconeIndex.query({
+        topK: 10,
+        vector: queryVector,
+        includeMetadata: true,
+    });
+
+    console.log(searchResults);
+
+
 }
 async function main(){
    const userProblem = readlineSync.question("Ask me anything--> ");
